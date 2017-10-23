4b3aad9198e2b35f250a0d9bb1e2894d67044331
Dave Carroll <djcarroll@gmail.com>
2017-10-23 14:39:03 -0700

    added carousel

--- scripted_install.sh
+++ scripted_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-sfdx force:org:create -s -a dxsdo -f config/project-scratch-def.json
-sfdx force:source:push
-sfdx force:user:permset:assign -n Recruiting
-sfdx force:apex:execute -f resetDataTables.apex
-sfdx force:data:tree:import -f Jobs__x.json 
-sfdx force:data:tree:import -p data/HR_Job_Requisition__c-HR_Applicant__c-plan.json
-sfdx force:apex:execute -f postPushSetup.apex
+cp -r ../../demo-testing/sdo-demo/certs certs
+sfdx force:org:create -a $1 -f config/project-scratch-def.json
+sfdx force:source:push -u $1
+sfdx force:user:permset:assign -n Recruiting -u $1
+sfdx force:apex:execute -f resetDataTables.apex -u $1
+sfdx force:data:tree:import -f Jobs__x.json -u $1
+sfdx force:data:tree:import -p data/HR_Job_Requisition__c-HR_Applicant__c-plan.json -u $1
+sfdx force:apex:execute -f postPushSetup.apex -u $1
