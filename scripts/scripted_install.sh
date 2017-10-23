ed67897d33c232c1c609943b5e41262d07d6344e
Dave Carroll <djcarroll@gmail.com>
2017-10-23 16:54:54 -0700

    organize scripts

--- scripts/scripted_install.sh
+++ scripts/scripted_install.sh
@@ -7,6 +7,6 @@ sfdx force:org:create -s -a dxsdo -f ../config/project-scratch-def.json
 sfdx force:source:push
 sfdx force:user:permset:assign -n Recruiting
 sfdx force:apex:execute -f ../resetDataTables.apex
-sfdx force:data:tree:import -f ../Jobs__c.json 
+sfdx force:data:tree:import -f ../data/Jobs__c.json 
 sfdx force:data:tree:import -p ../data/HR_Job_Requisition__c-HR_Applicant__c-plan.json
 sfdx force:apex:execute -f ../postPushSetup.apex
